b2a7dac4167dddc58622bf0d8d550202bc7e44fc
vin0414 <vinmogate@gmail.com>
2023-10-28 21:14:05 +0800

    add pages

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,14 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function membership()
+    {
+        return view('membership');
+    }
+
+    public function book()
+    {
+        return view('book');
+    }
 }
