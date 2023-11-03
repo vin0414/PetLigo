6e2175292bb7865a59e552b2ec9a09c49801dad8
vin0414 <vinmogate@gmail.com>
2023-11-03 18:10:42 +0800

    add cart system

--- app/Controllers/Cart.php
+++ app/Controllers/Cart.php
@@ -4,5 +4,33 @@ namespace App\Controllers;
 
 class Cart extends BaseController
 {
+    public function viewCart()
+    {
+        $data['items'] = array_values(session('cart'));
+        return view('view-cart',$data);
+    }
 
+    public function buy($id)
+    {
+        $productModel = new \App\Models\productModel();
+        $product = $productModel->find($id);
+        $item = array(
+            'id'=>$product['productID'],
+            'name'=>$product['productName'],
+            'photo'=>$product['Image'],
+            'price'=>$product['UnitPrice'],
+            'quantity'=>1
+        );
+        $session = session();
+        if($session->has('cart'))
+        {
+
+        }
+        else
+        {
+            $cart = array($item);
+            $session->set('cart',$cart);
+        }
+        return $this->response->redirect(site_url('view-cart'));
+    }
 }
