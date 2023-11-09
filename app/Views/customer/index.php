e6628037e70ef8baca622e1578adcb7c06419b63
vin0414 <vinmogate@gmail.com>
2023-11-09 09:30:11 +0800

    add pages in customer

--- app/Views/customer/index.php
+++ app/Views/customer/index.php
@@ -385,45 +385,27 @@
 				<div class="sidebar-menu">
 					<ul id="accordion-menu">
 						<li class="dropdown">
-							<a href="<?=site_url('admin/dashboard')?>" class="dropdown-toggle active no-arrow">
-								<span class="micon bi bi-house"></span
-								><span class="mtext">Home</span>
+							<a href="<?=site_url('customer/dashboard')?>" class="dropdown-toggle active no-arrow">
+								<span class="micon bi bi-grid"></span
+								><span class="mtext">Dashboard</span>
+							</a>
+						</li>
+                        <li class="dropdown">
+							<a href="<?=site_url('products')?>" class="dropdown-toggle no-arrow">
+								<span class="micon bi bi-shop"></span>
+                                <span class="mtext">Shop Now</span>
 							</a>
 						</li>
 						<li class="dropdown">
-							<a href="<?=site_url('admin/reservations')?>" class="dropdown-toggle no-arrow">
+							<a href="<?=site_url('customer/reservations')?>" class="dropdown-toggle no-arrow">
 								<span class="micon dw dw-calendar-1"></span
 								><span class="mtext"> Reservations </span>
 							</a>
 						</li>
 						<li class="dropdown">
-							<a href="<?=site_url('admin/orders')?>" class="dropdown-toggle no-arrow">
+							<a href="<?=site_url('customer/orders')?>" class="dropdown-toggle no-arrow">
 								<span class="micon dw dw-shopping-cart"></span
 								><span class="mtext">Orders</span>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a href="<?=site_url('admin/products')?>" class="dropdown-toggle no-arrow">
-								<span class="micon dw dw-clipboard"></span
-								><span class="mtext">Products</span>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a href="<?=site_url('admin/membership')?>" class="dropdown-toggle no-arrow">
-								<span class="micon dw dw-user-13"></span
-								><span class="mtext">Membership</span>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a href="<?=site_url('admin/reports')?>" class="dropdown-toggle no-arrow">
-								<span class="micon bi bi-pie-chart"></span
-								><span class="mtext">Reports</span>
-							</a>
-						</li>						
-						<li class="dropdown">
-							<a href="<?=site_url('admin/maintenance')?>" class="dropdown-toggle no-arrow">
-								<span class="micon dw dw-settings"></span
-								><span class="mtext">Maintenance</span>
 							</a>
 						</li>
 							<div class="dropdown-divider"></div>
@@ -432,7 +414,7 @@
 							<div class="sidebar-small-cap">Extra</div>
 						</li>
                         <li>
-							<a href="<?=site_url('admin/profile')?>" class="dropdown-toggle no-arrow">
+							<a href="<?=site_url('customer/profile')?>" class="dropdown-toggle no-arrow">
 								<span class="micon dw dw-user2"></span
 								><span class="mtext">Profile</span>
 							</a>
@@ -463,307 +445,6 @@
 							</p>
 						</div>
 					</div>
-				</div>
-				<div class="row">
-					<div class="col-xl-3 mb-30">
-						<div class="card-box height-100-p widget-style1">
-							<div class="d-flex flex-wrap align-items-center">
-								<div class="progress-data">
-									<div id="chart"></div>
-								</div>
-								<div class="widget-data">
-									<div class="h4 mb-0">2020</div>
-									<div class="weight-600 font-14">Contact</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-3 mb-30">
-						<div class="card-box height-100-p widget-style1">
-							<div class="d-flex flex-wrap align-items-center">
-								<div class="progress-data">
-									<div id="chart2"></div>
-								</div>
-								<div class="widget-data">
-									<div class="h4 mb-0">400</div>
-									<div class="weight-600 font-14">Deals</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-3 mb-30">
-						<div class="card-box height-100-p widget-style1">
-							<div class="d-flex flex-wrap align-items-center">
-								<div class="progress-data">
-									<div id="chart3"></div>
-								</div>
-								<div class="widget-data">
-									<div class="h4 mb-0">350</div>
-									<div class="weight-600 font-14">Campaign</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-3 mb-30">
-						<div class="card-box height-100-p widget-style1">
-							<div class="d-flex flex-wrap align-items-center">
-								<div class="progress-data">
-									<div id="chart4"></div>
-								</div>
-								<div class="widget-data">
-									<div class="h4 mb-0">$6060</div>
-									<div class="weight-600 font-14">Worth</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-xl-8 mb-30">
-						<div class="card-box height-100-p pd-20">
-							<h2 class="h4 mb-20">Activity</h2>
-							<div id="chart5"></div>
-						</div>
-					</div>
-					<div class="col-xl-4 mb-30">
-						<div class="card-box height-100-p pd-20">
-							<h2 class="h4 mb-20">Lead Target</h2>
-							<div id="chart6"></div>
-						</div>
-					</div>
-				</div>
-				<div class="card-box mb-30">
-					<h2 class="h4 pd-20">Best Selling Products</h2>
-					<table class="data-table table nowrap">
-						<thead>
-							<tr>
-								<th class="table-plus datatable-nosort">Product</th>
-								<th>Name</th>
-								<th>Color</th>
-								<th>Size</th>
-								<th>Price</th>
-								<th>Oty</th>
-								<th class="datatable-nosort">Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td class="table-plus">
-									<img
-										src="vendors/images/product-1.jpg"
-										width="70"
-										height="70"
-										alt=""
-									/>
-								</td>
-								<td>
-									<h5 class="font-16">Shirt</h5>
-									by John Doe
-								</td>
-								<td>Black</td>
-								<td>M</td>
-								<td>$1000</td>
-								<td>1</td>
-								<td>
-									<div class="dropdown">
-										<a
-											class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-											href="#"
-											role="button"
-											data-toggle="dropdown"
-										>
-											<i class="dw dw-more"></i>
-										</a>
-										<div
-											class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
-										>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-eye"></i> View</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-edit2"></i> Edit</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-delete-3"></i> Delete</a
-											>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td class="table-plus">
-									<img
-										src="vendors/images/product-2.jpg"
-										width="70"
-										height="70"
-										alt=""
-									/>
-								</td>
-								<td>
-									<h5 class="font-16">Boots</h5>
-									by Lea R. Frith
-								</td>
-								<td>brown</td>
-								<td>9UK</td>
-								<td>$900</td>
-								<td>1</td>
-								<td>
-									<div class="dropdown">
-										<a
-											class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-											href="#"
-											role="button"
-											data-toggle="dropdown"
-										>
-											<i class="dw dw-more"></i>
-										</a>
-										<div
-											class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
-										>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-eye"></i> View</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-edit2"></i> Edit</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-delete-3"></i> Delete</a
-											>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td class="table-plus">
-									<img
-										src="vendors/images/product-3.jpg"
-										width="70"
-										height="70"
-										alt=""
-									/>
-								</td>
-								<td>
-									<h5 class="font-16">Hat</h5>
-									by Erik L. Richards
-								</td>
-								<td>Orange</td>
-								<td>M</td>
-								<td>$100</td>
-								<td>4</td>
-								<td>
-									<div class="dropdown">
-										<a
-											class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-											href="#"
-											role="button"
-											data-toggle="dropdown"
-										>
-											<i class="dw dw-more"></i>
-										</a>
-										<div
-											class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
-										>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-eye"></i> View</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-edit2"></i> Edit</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-delete-3"></i> Delete</a
-											>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td class="table-plus">
-									<img
-										src="vendors/images/product-4.jpg"
-										width="70"
-										height="70"
-										alt=""
-									/>
-								</td>
-								<td>
-									<h5 class="font-16">Long Dress</h5>
-									by Renee I. Hansen
-								</td>
-								<td>Gray</td>
-								<td>L</td>
-								<td>$1000</td>
-								<td>1</td>
-								<td>
-									<div class="dropdown">
-										<a
-											class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-											href="#"
-											role="button"
-											data-toggle="dropdown"
-										>
-											<i class="dw dw-more"></i>
-										</a>
-										<div
-											class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
-										>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-eye"></i> View</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-edit2"></i> Edit</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-delete-3"></i> Delete</a
-											>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td class="table-plus">
-									<img
-										src="vendors/images/product-5.jpg"
-										width="70"
-										height="70"
-										alt=""
-									/>
-								</td>
-								<td>
-									<h5 class="font-16">Blazer</h5>
-									by Vicki M. Coleman
-								</td>
-								<td>Blue</td>
-								<td>M</td>
-								<td>$1000</td>
-								<td>1</td>
-								<td>
-									<div class="dropdown">
-										<a
-											class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-											href="#"
-											role="button"
-											data-toggle="dropdown"
-										>
-											<i class="dw dw-more"></i>
-										</a>
-										<div
-											class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
-										>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-eye"></i> View</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-edit2"></i> Edit</a
-											>
-											<a class="dropdown-item" href="#"
-												><i class="dw dw-delete-3"></i> Delete</a
-											>
-										</div>
-									</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
 				</div>
 			</div>
 		</div>
