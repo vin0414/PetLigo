61f47e43e8117521ae21cf667fee9db02f4dd0f8
vin0414 <vinmogate@gmail.com>
2023-12-01 10:12:29 +0800

    update the landing page

--- app/Views/services.php
+++ app/Views/services.php
@@ -79,53 +79,6 @@
         </div>
       </div>
     </section>
-
-    <section class="ftco-section ftco-no-pt ftco-no-pb">
-    	<div class="container">
-    		<div class="row d-flex no-gutters">
-    			<div class="col-md-5 d-flex">
-    				<div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0" style="background-image:url(assets/images/about-1.jpg);">
-    				</div>
-    			</div>
-    			<div class="col-md-7 pl-md-5 py-md-5">
-    				<div class="heading-section pt-md-5">
-	            <h2 class="mb-4">Why Choose Us?</h2>
-    				</div>
-    				<div class="row">
-	    				<div class="col-md-6 services-2 w-100 d-flex">
-	    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-stethoscope"></span></div>
-	    					<div class="text pl-3">
-	    						<h4>Care Advices</h4>
-	    						<p>Far far away, behind the word mountains, far from the countries.</p>
-	    					</div>
-	    				</div>
-	    				<div class="col-md-6 services-2 w-100 d-flex">
-	    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-customer-service"></span></div>
-	    					<div class="text pl-3">
-	    						<h4>Customer Supports</h4>
-	    						<p>Far far away, behind the word mountains, far from the countries.</p>
-	    					</div>
-	    				</div>
-	    				<div class="col-md-6 services-2 w-100 d-flex">
-	    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-emergency-call"></span></div>
-	    					<div class="text pl-3">
-	    						<h4>Emergency Services</h4>
-	    						<p>Far far away, behind the word mountains, far from the countries.</p>
-	    					</div>
-	    				</div>
-	    				<div class="col-md-6 services-2 w-100 d-flex">
-	    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-veterinarian"></span></div>
-	    					<div class="text pl-3">
-	    						<h4>Veterinary Help</h4>
-	    						<p>Far far away, behind the word mountains, far from the countries.</p>
-	    					</div>
-	    				</div>
-	    			</div>
-	        </div>
-        </div>
-    	</div>
-    </section>
-
     <section class="ftco-section bg-light">
     	<div class="container">
     		<div class="row mb-5 pb-5">
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -316,81 +316,45 @@
 				<div class="row">
           <div class="col-md-4 ftco-animate">
             <div class="work mb-4 img d-flex align-items-end" style="background-image: url(assets/images/gallery-1.jpg);">
-            	<a href="images/gallery-1.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+            	<a href="assets/images/gallery-1.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
 	    					<span class="fa fa-expand"></span>
 	    				</a>
-            	<div class="desc w-100 px-4">
-	              <div class="text w-100 mb-3">
-	              	<span>Cat</span>
-	              	<h2><a href="work-single.html">Persian Cat</a></h2>
-	              </div>
-              </div>
             </div>
           </div>
           <div class="col-md-4 ftco-animate">
             <div class="work mb-4 img d-flex align-items-end" style="background-image: url(assets/images/gallery-2.jpg);">
-            	<a href="images/gallery-2.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+            	<a href="assets/images/gallery-2.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
 	    					<span class="fa fa-expand"></span>
 	    				</a>
-            	<div class="desc w-100 px-4">
-	              <div class="text w-100 mb-3">
-	              	<span>Dog</span>
-	              	<h2><a href="work-single.html">Pomeranian</a></h2>
-	              </div>
-              </div>
             </div>
           </div>
           <div class="col-md-4 ftco-animate">
             <div class="work mb-4 img d-flex align-items-end" style="background-image: url(assets/images/gallery-3.jpg);">
-            	<a href="images/gallery-3.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+            	<a href="assets/images/gallery-3.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
 	    					<span class="fa fa-expand"></span>
 	    				</a>
-            	<div class="desc w-100 px-4">
-	              <div class="text w-100 mb-3">
-	              	<span>Cat</span>
-	              	<h2><a href="work-single.html">Sphynx Cat</a></h2>
-	              </div>
-              </div>
             </div>
           </div>
 
           <div class="col-md-4 ftco-animate">
             <div class="work mb-4 img d-flex align-items-end" style="background-image: url(assets/images/gallery-4.jpg);">
-            	<a href="images/gallery-4.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+            	<a href="assets/images/gallery-4.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
 	    					<span class="fa fa-expand"></span>
 	    				</a>
-            	<div class="desc w-100 px-4">
-	              <div class="text w-100 mb-3">
-	              	<span>Cat</span>
-	              	<h2><a href="work-single.html">British Shorthair</a></h2>
-	              </div>
-              </div>
             </div>
           </div>
           <div class="col-md-4 ftco-animate">
             <div class="work mb-4 img d-flex align-items-end" style="background-image: url(assets/images/gallery-5.jpg);">
-            	<a href="images/gallery-5.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+            	<a href="assets/images/gallery-5.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
 	    					<span class="fa fa-expand"></span>
 	    				</a>
-            	<div class="desc w-100 px-4">
-	              <div class="text w-100 mb-3">
-	              	<span>Dog</span>
-	              	<h2><a href="work-single.html">Beagle</a></h2>
-	              </div>
-              </div>
             </div>
           </div>
           <div class="col-md-4 ftco-animate">
             <div class="work mb-4 img d-flex align-items-end" style="background-image: url(assets/images/gallery-6.jpg);">
-            	<a href="images/gallery-6.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+            	<a href="assets/images/gallery-6.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
 	    					<span class="fa fa-expand"></span>
 	    				</a>
-            	<div class="desc w-100 px-4">
-	              <div class="text w-100 mb-3">
-	              	<span>Dog</span>
-	              	<h2><a href="work-single.html">Pug</a></h2>
-	              </div>
-              </div>
             </div>
           </div>
         </div>
