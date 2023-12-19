3e385a7aa7ac995254f4c93d6e103d4548cd78b8
vin0414 <vinmogate@gmail.com>
2023-12-19 17:26:28 +0800

    update

--- app/Views/admin/view.php
+++ app/Views/admin/view.php
@@ -466,6 +466,7 @@
 				</div>
 			</div>
 		</div>
+
 		<div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
 			<div class="modal-dialog modal-dialog-centered">
 				<div class="modal-content">
