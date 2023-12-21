034618c97ff2407a333521664efb9f251f89a01d
vin0414 <vinmogate@gmail.com>
2023-12-21 11:40:55 +0800

    remove notifications

--- app/Views/customer/profile.php
+++ app/Views/customer/profile.php
@@ -74,37 +74,6 @@
 				<div class="menu-icon bi bi-list"></div>
 			</div>
 			<div class="header-right">
-				<div class="dashboard-setting user-notification">
-					<div class="dropdown">
-						<a
-							class="dropdown-toggle no-arrow"
-							href="javascript:;"
-							data-toggle="right-sidebar"
-						>
-							<i class="dw dw-settings2"></i>
-						</a>
-					</div>
-				</div>
-				<div class="user-notification">
-					<div class="dropdown">
-						<a
-							class="dropdown-toggle no-arrow"
-							href="#"
-							role="button"
-							data-toggle="dropdown"
-						>
-							<i class="icon-copy dw dw-notification"></i>
-							<span class="badge notification-active"></span>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right">
-							<div class="notification-list mx-h-350 customscroll">
-								<ul>
-									
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
 				<div class="user-info-dropdown">
 					<div class="dropdown">
 						<a
